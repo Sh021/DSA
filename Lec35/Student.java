77009a1ce1f2e3b0d89ed93d2156eccb997846fb
Shubham <Shubham.glenhill@gmail.com>
2022-12-05 19:48:21 +0530

    lec

--- Lec35/Student.java
+++
@@ -1,9 +0,0 @@
-package Lec35;
-
-public class Student {
- String name;
- int age;
- public void introyourself() {
-	 System.out.println("My name is "+name+" and my age is "+age+" years old.");
- }
-}
